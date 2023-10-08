bd6da976ae4de973c264cad00748644113ff9f3f
Jan-Thomas Riemenschneider <jt.riemenschneider@gmail.com>
2023-10-08 17:02:40 +0200

    uwu

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # OKC Like Revealer
+## How to use
+1. Visit https://www.okcupid.com/who-likes-you.  
+2. Press F12 to access the DevTools and switch to "Console".  
+3. Wait for the site to load completely.  
+4. Paste in the script and hit enter.
+
+The raw script can be found here: https://raw.githubusercontent.com/dajanto/okclikerevealer/main/revealer.js?token=GHSAT0AAAAAACFL3WEZANHJ3HNIIV6X2ZUMZJCYHOQ 
+
+## How it works
+It removes the paywall popup first, then creates an image container for every image url of every contact and appends it to the container which holds the contacts in the first place (and removes the old ones). If you want to undo it, simply refresh the site.
--- revealer.js
+++ revealer.js
@@ -1,15 +1,13 @@
-document.addEventListener("DOMSubtreeModified", function (event) {
-    let paywall = document.querySelector('.likes-you-paywall-explainer-cta');
-    paywall.remove();
-    let placeholderElements = document.querySelectorAll(".usercard-placeholder-thumb");
-    let likes = document.querySelector('.likes-you-paywall-with-likes');
-    likes.remove();
-    let cont = document.querySelector('.userrows-main');
-    placeholderElements.forEach(function (element) {
-        let url = styleAttribute = element.getAttribute('style').match('https.*jpeg');
-        let imgElement = document.createElement("img");
-        imgElement.setAttribute("src", url);
-        cont.appendChild(imgElement);
-        element.remove();
-    });
+let paywall = document.querySelector('.likes-you-paywall-explainer-cta');
+paywall.remove();
+let placeholderElements = document.querySelectorAll(".usercard-placeholder-thumb");
+let likes = document.querySelector('.likes-you-paywall-with-likes');
+likes.remove();
+let cont = document.querySelector('.userrows-main');
+placeholderElements.forEach(function (element) {
+    let url = styleAttribute = element.getAttribute('style').match('https.*jpeg');
+    let imgElement = document.createElement("img");
+    imgElement.setAttribute("src", url);
+    cont.appendChild(imgElement);
+    element.remove();
 });
