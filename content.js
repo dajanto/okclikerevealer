6ff1e26ed07aa958a69d8c89a3a43e080aa5c50a
Jan-Thomas Riemenschneider <jt.riemenschneider@gmail.com>
2023-10-08 16:38:47 +0200

    uwu

--- content.js
+++ content.js
@@ -1,14 +1,15 @@
-let paywall = document.querySelector('.likes-you-paywall-explainer-cta');
-paywall.remove();
-let placeholderElements = document.querySelectorAll(".usercard-placeholder-thumb");
-let likes = document.querySelector('.likes-you-paywall-with-likes');
-likes.remove();
-let cont = document.querySelector('.userrows-main');
-placeholderElements.forEach(function (element) {
-    let url = styleAttribute = element.getAttribute('style').match('https.*jpeg');
-    let imgElement = document.createElement("img");
-    imgElement.setAttribute("src", url);
-    cont.appendChild(imgElement);
-    element.remove();
+document.addEventListener("DOMSubtreeModified", function (event) {
+    let paywall = document.querySelector('.likes-you-paywall-explainer-cta');
+    paywall.remove();
+    let placeholderElements = document.querySelectorAll(".usercard-placeholder-thumb");
+    let likes = document.querySelector('.likes-you-paywall-with-likes');
+    likes.remove();
+    let cont = document.querySelector('.userrows-main');
+    placeholderElements.forEach(function (element) {
+        let url = styleAttribute = element.getAttribute('style').match('https.*jpeg');
+        let imgElement = document.createElement("img");
+        imgElement.setAttribute("src", url);
+        cont.appendChild(imgElement);
+        element.remove();
+    });
 });
-ndChild (s);
