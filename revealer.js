4da52f9783ad32ce71db3c32ef66cd6596096d35
Jan-Thomas Riemenschneider <jt.riemenschneider@gmail.com>
2023-10-11 20:50:41 +0200

    attribute fix

--- revealer.js
+++ revealer.js
@@ -5,7 +5,7 @@ let likes = document.querySelector('.likes-you-paywall-with-likes');
 likes.remove();
 let cont = document.querySelector('.userrows-main');
 placeholderElements.forEach(function (element) {
-    let url = styleAttribute = element.getAttribute('style').match('https.*jpeg');
+    let url = element.getAttribute('style').match('https.*jpeg');
     let imgElement = document.createElement("img");
     imgElement.setAttribute("src", url);
     cont.appendChild(imgElement);
